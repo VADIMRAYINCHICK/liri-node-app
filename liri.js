f23491d18d2643cc95ffa412ecde82d2f163874b
VADIMRAYINCHICK <VADIM.RAYINCHICK@GMAIL.COM>
2017-04-28 17:46:23 -0700

    spotify update

--- liri.js
+++ liri.js
@@ -1,13 +1,39 @@
 
 const keys = require("./keys.js");
 const Twitter = require('twitter');
+const spotify = require('spotify');
+
+
 
 const consKey = keys.twitterKeys.consumer_key
 const consSec = keys.twitterKeys.consumer_secret
 const tokKEY = keys.twitterKeys.access_token_key
 const tokSec = keys.twitterKeys.access_token_secret
 
+const userInput = process.argv[2];
 
+if (userInput === "my-tweets"){
+	myTweets()
+}
+
+if (userInput === "spotify-this-song"){
+	spotifySearch()
+}
+
+if (userInput === "movie-this"){
+	movie()
+}
+
+if (userInput === "do-what-it-says"){
+	doWhat()
+}
+
+
+
+
+
+function myTweets() {
+	// body...
 
 const client = new Twitter({
   consumer_key: consKey,
@@ -17,21 +43,11 @@ const client = new Twitter({
 });
 
 
-client.post('statuses/update', {status: 'I am tweeting'}, function(error, tweet, response) {
-  if (!error) {
-    console.log(tweet);
-  }
-});
-// client.post('statuses/update', {status: 'I am testing Twitter'},  function(error, tweet, response) {
-//   if(error) throw error;
-//   console.log(tweet);  // Tweet body. 
-//   console.log(response);  // Raw response object. 
-// });
 
 client.get('statuses/user_timeline', {q: 'node.js'}, function(error, tweets, response) {
    if (!error) {
 
-  	 for (var i = 0; i < 20; i++) {
+  	 for (let i = 0; i < 20; i++) {
   		
   		var num =i+1;
         console.log("Tweet number "+ num+ " was created on "+tweets[i].created_at)
@@ -40,4 +56,81 @@ client.get('statuses/user_timeline', {q: 'node.js'}, function(error, tweets, res
   	 }
   	
    }
-});
+})
+
+}
+
+function spotifySearch() {
+
+	let songInput = process.argv[3];
+
+	if (songInput === ""){
+		spotify.search({ type: 'track', query: "ace of base" }, function(err, data) {
+    if (!err) {
+        
+        const song = data.tracks.items
+
+        for (var i = 0; i < song.length; i++) {
+      
+ 
+    console.log("Artist of this song is: " + song[i].artists[0].name+"\n")
+    console.log("Name of this song is: " + song[i].name+"\n")
+    console.log("Link of this song is: " + song[i].external_urls.spotify+"\n")
+    console.log("The album that song was on is: " + song[i].album.name+"\n")
+    console.log('-----------------------------------------------------------------------------------')
+    
+}
+
+	}
+
+    else {
+    	throw err
+        process.exit(0);
+    }
+
+})
+	}
+
+spotify.search({ type: 'track', query: songInput }, function(err, data) {
+    if (!err) {
+        
+        const song = data.tracks.items
+
+        for (var i = 0; i < song.length; i++) {
+      
+ 
+    console.log("Artist of this song is: " + song[i].artists[0].name+"\n")
+    console.log("Name of this song is: " + song[i].name+"\n")
+    console.log("Link of this song is: " + song[i].external_urls.spotify+"\n")
+    console.log("The album that song was on is: " + song[i].album.name+"\n")
+    console.log('-----------------------------------------------------------------------------------')
+    console.log("you got: "+song.length+" results in total")
+}
+
+    
+
+
+	}
+
+    else {
+    	throw err
+        process.exit(0);
+    }
+
+})
+
+}
+	// body...
+
+
+// function movie() {
+
+
+// 	// body...
+// }
+
+// function doWhat() {
+
+
+// 	// body...
+// }
